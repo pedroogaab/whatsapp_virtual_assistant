3888625502c78bb74a0d1987ec28bb99620b4013
pedroogaab <gdspedro.ml@gmail.com>
2025-07-05 20:09:37 -0300

    add a context AI personality

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ import json
 from ollama import chat
 from ollama import ChatResponse
 
+
 # Run Redis server locally
 pool = redis.ConnectionPool(host="localhost", port=6379, db=0)
 r = redis.Redis(
-    connection_pool=pool, decode_responses=True
-)  # Decode responses to strings
+    connection_pool=pool, decode_responses=True  # Decode responses to strings
+)
 
 app = Flask(__name__)
 
@@ -20,28 +21,36 @@ def send_message():
     user_message = data.get("message")
     to = data.get("to")
 
-    # Use 10 last msgs for get the context of conversation
-    last_10_message = r.lrange(to, -10, -1)
+    # Use 2 last msgs for get the context of conversation
+    last_2_message = r.lrange(to, -2, -1)
     history = []
-    for msg in last_10_message:
+
+    for msg in last_2_message:
         msg_data = json.loads(msg)
         history.append({"role": "user", "content": msg_data["user"]})
         history.append({"role": "assistant", "content": msg_data["ai"]})
 
-    # Group the new msg with the history
+    # Group the new user msg with the history
     history.append({"role": "user", "content": user_message})
-
+    # Define a personality for the AI agent
+    system = """Você é um assistente financeiro virtual que responde de forma clara e objetiva, com respostas curtas concisas e diretas. O idioma falado deve ser o português brasileiro."""
+    # Combine the personality with the conversation history and the new user message
+    prompt = [
+        {"role": "system", "content": system},
+        *history,
+    ]  # *history unpacks the list
     response: ChatResponse = chat(
         model="llama3.2",
-        messages=history,
+        messages=prompt,
         stream=False,
     )
 
     ai_response = response["message"]["content"]
-
     r.rpush(to, json.dumps({"user": user_message, "ai": ai_response}))
 
-    return jsonify({"to": to, "response": ai_response})
+    return jsonify(
+        {"AI Agent": ai_response}
+    )  # jsonify convert the response to JSON format
 
 
 # Get the last 10 messages of a conversation
@@ -51,7 +60,7 @@ def get_messages():
     to = request.args.get("to")
     messages = r.lrange(to, -10, -1)
     messages = [json.loads(msg) for msg in messages]
-    return jsonify({"to": to, "messages": messages})
+    return jsonify({"messages": messages})
 
 
 if __name__ == "__main__":
